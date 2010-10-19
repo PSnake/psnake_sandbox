7ead1dd208a9a3a6e903a1aebfd5a6819ca5f54f
PSnake <pedro.pbs@gmail.com>
2010-10-19 01:41:14 +0100

    added ybed project

---
+++ ybed/ybedApp.cpp
@@ -0,0 +1,33 @@
+/***************************************************************
+ * Name:      ybedApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    Pedro Sa (pedro.pbs@gmail.com)
+ * Created:   2010-10-18
+ * Copyright: Pedro Sa ()
+ * License:
+ **************************************************************/
+
+#include "ybedApp.h"
+
+//(*AppHeaders
+#include "ybedMain.h"
+#include <wx/image.h>
+//*)
+
+IMPLEMENT_APP(ybedApp);
+
+bool ybedApp::OnInit()
+{
+    //(*AppInitialize
+    bool wxsOK = true;
+    wxInitAllImageHandlers();
+    if ( wxsOK )
+    {
+    	ybedFrame* Frame = new ybedFrame(0);
+    	Frame->Show();
+    	SetTopWindow(Frame);
+    }
+    //*)
+    return wxsOK;
+
+}
---
+++ ybed/ybedMain.h
@@ -0,0 +1,51 @@
+/***************************************************************
+ * Name:      ybedMain.h
+ * Purpose:   Defines Application Frame
+ * Author:    Pedro Sa (pedro.pbs@gmail.com)
+ * Created:   2010-10-18
+ * Copyright: Pedro Sa ()
+ * License:
+ **************************************************************/
+
+#ifndef YBEDMAIN_H
+#define YBEDMAIN_H
+
+//(*Headers(ybedFrame)
+#include <wx/menu.h>
+#include <wx/textctrl.h>
+#include <wx/frame.h>
+#include <wx/statusbr.h>
+//*)
+
+class ybedFrame: public wxFrame
+{
+    public:
+
+        ybedFrame(wxWindow* parent,wxWindowID id = -1);
+        virtual ~ybedFrame();
+
+    private:
+
+        //(*Handlers(ybedFrame)
+        void OnQuit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+        void OnSave(wxCommandEvent& event);
+        //*)
+
+        //(*Identifiers(ybedFrame)
+        static const long ID_TEXTCTRL1;
+        static const long idMenuSave;
+        static const long idMenuQuit;
+        static const long idMenuAbout;
+        static const long ID_STATUSBAR1;
+        //*)
+
+        //(*Declarations(ybedFrame)
+        wxStatusBar* StatusBar1;
+        wxTextCtrl* TextCtrl1;
+        //*)
+
+        DECLARE_EVENT_TABLE()
+};
+
+#endif // YBEDMAIN_H
